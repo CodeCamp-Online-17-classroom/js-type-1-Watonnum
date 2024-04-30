d78ce3a0c69bb63d676aae6cc570c31f186e9ffb
Watonnum <adamcorktail@gmail.com>
2024-04-30 17:08:01 +0700

    Done

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// write code here
+let a = undefined;
+let b = 2;
+let c = a++;
+
+let d = String(a);
+let e = String(b);
+let f = String(c);
+
+console.log(d);
+console.log(e);
+console.log(f);
+
+// d = NaN
+// e = 2
+// f = NaN
+// เพราะค่า e เป็น number จึงเป็นตัวแปรเดียวที่สามารถแปลงให้เป็น String ได้
+// ปล. NaN = not a number
